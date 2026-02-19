3382661f1c7ae65e123549e10e2aa247f61557f8
madarsh07 <adarshmendhe101995@gmail.com>
2026-02-19 22:16:55 +0530

    statefile s3

---
+++ Day3/main.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = var.security_group_name
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_http_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "mylabbucket" {
+  bucket = "terraformstatefilecewnuiwenv" # Must be globally unique
+
+  tags = {
+    Name        = "MyLabBucket"
+
+  }
+}
