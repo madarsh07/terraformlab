bde8d4b702df1cdbde158d236a3a972dcfb730c4
madarsh07 <adarshmendhe101995@gmail.com>
2026-02-23 12:28:19 +0530

    Update output.tf

--- Day4_VPC/output.tf
+++ Day4_VPC/output.tf
@@ -14,10 +14,6 @@ output "public_ec2_public_ip" {
   value = aws_instance.public_ec2.public_ip
 }
 
-output "public_ec2_private_ip" {
-  value = aws_instance.public_ec2.private_ip
-}
-
 output "private_ec2_private_ip" {
   value = aws_instance.private_ec2.private_ip
 }
