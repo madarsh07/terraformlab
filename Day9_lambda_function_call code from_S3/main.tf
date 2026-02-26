fa1cf7e99b9e11ff5a496d0ec934bd6fc03d317f
madarsh07 <adarshmendhe101995@gmail.com>
2026-02-26 11:31:02 +0530

    all

--- Day9_lambda_function_call code from_S3/main.tf
+++ Day9_lambda_function_call code from_S3/main.tf
@@ -17,41 +17,15 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 ############################
-# IAM Policy
+# Attach Basic Execution Role
 ############################
-resource "aws_iam_policy" "lambda_s3_policy" {
-  name = "lambda_s3_read_policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject"
-        ],
-        Resource = "arn:aws:s3:::your-existing-bucket-name/*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_policy" {
+resource "aws_iam_role_policy_attachment" "basic_execution" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 ############################
-# Lambda Function
+# Lambda Function (Code From S3)
 ############################
 resource "aws_lambda_function" "zip_reader" {
   function_name = "existing_s3_zip_reader"
@@ -59,16 +33,9 @@ resource "aws_lambda_function" "zip_reader" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
 
-  filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  s3_bucket = "enwdevlabtest"
+  s3_key    = "lambda_function.zip"
 
   timeout     = 60
   memory_size = 256
-
-  environment {
-    variables = {
-      BUCKET_NAME = "your-existing-bucket-name"
-      ZIP_KEY     = "your-zip-file.zip"
-    }
-  }
 }
