6cc196cabdbdf8a707949e8273f6d3c6df795229
madarsh07 <adarshmendhe101995@gmail.com>
2026-03-18 22:24:32 +0530

    add

---
+++ terraform-multi-providers/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "name" {
+  ami="ami-07860a2d7eb515d9a" 
+  instance_type = "t2.micro"
+
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "tybuwbdsjbcscj"
+    provider = aws.oregon
+  
+}
